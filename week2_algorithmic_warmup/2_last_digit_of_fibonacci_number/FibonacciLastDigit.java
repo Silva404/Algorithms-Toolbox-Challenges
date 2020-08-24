eb896609cdc857276d55a5f58773cf7f3cdee812
Silva404 <brenosilva0252@gmail.com>
2020-08-24 08:25:29 -0300

    fibonacci last digit

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -1,26 +1,43 @@
-import java.util.*;
-
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigitNaive(int n) {
         if (n <= 1)
             return n;
 
-        int previous = 0;
-        int current  = 1;
+        int previous = 0; 
+        int current  = 1; 
 
         for (int i = 0; i < n - 1; ++i) {
-            int tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
+            int tmp_previous = previous; 
+            previous = current; 
+            current = tmp_previous + current; 
         }
 
         return current % 10;
     }
+
+    void findFib(int f[]) {
+        f[0] = 0;
+        f[1] = 1;
+
+        for (int i = 2; i <= 59; i++) {
+            f[i] = (f[i - 1] + f[i - 2]) % 10; 
+        }
+    }
     
+    int fibLastDigit(long n) {
+        int f[] = new int[60];
+
+        findFib(f);
+
+        int index = (int)(n % 60L);
+
+        return f[index];
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int c = getFibonacciLastDigitNaive(n);
+        int n = 15;
+        int c = fibLastDigit(n);
+        // int c = getFibonacciLastDigitNaive(n);
         System.out.println(c);
     }
 }
--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.js
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.js
@@ -1,21 +1,33 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    console.log(fib(parseInt(line, 10)));
-    process.exit();
+  console.log(fib(parseInt(line, 10)));
+  process.exit();
 }
 
 function fib(n) {
-    // write your code here
+  if (n <= 1) return n;
+
+  let previous = 0;
+  let current = 1;
+
+  for (let i = 0; i < n - 1; i++) {
+    let tmp_previous = previous;
+    previous = current;
+    current = tmp_previous + current;
+  }
+
+  return current % 10;
 }
+console.log(fib(15));
 
 module.exports = fib;
