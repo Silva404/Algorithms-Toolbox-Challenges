0a3907ff1bab1deb57cf33dff61a4a20b9be9760
Silva404 <brenosilva0252@gmail.com>
2020-08-23 22:26:11 -0300

    fibonnaci fast solution

--- week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
+++ week2_algorithmic_warmup/1_fibonacci_number/Fibonacci.java
@@ -1,24 +1,42 @@
-import java.util.Scanner;
-
-public class Fibonacci {
-  private static long calc_fib(int n) {
+class Fibonacci {
+  // private static long naiveCalc_fib(int n) {
     
-    if (n <= 1)
-      return n;
+  //   if (n <= 1)
+  //     return n;
 
-    return calc_fib(n - 1) + calc_fib(n - 2);
+  //   return calc_fib(n - 1) + calc_fib(n - 2);
+  // }
+
+  static int fib(int n) {
+    int f[] = new int[n + 2];
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      f[i] = f[i - 1] + f[i - 2];
+    }
+
+    return f[n];
   }
 
-  public static long FibList(int n) {
-    int n[0] = 0
-    int n[1] = 1
-    
-    return 
+  static int evenFaster(int n) {
+    int a = 0, b = 1, c, i;
+
+    if (n == 0) return a;
+
+    for (i = 2; i <= n; i++){
+      c = a + b;
+      a = b;
+      b = c;
+    }
+
+    return b;
   }
+  
   public static void main(String args[]) {
-    // Scanner in = new Scanner(System.in);
-    // int n = in.nextInt();
-    
-    System.out.println(calc_fib(40));
+    int n = 200;
+
+    System.out.println(evenFaster(n));
+    System.out.println(fib(n));
   }
 }
--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
@@ -1,25 +1,42 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    console.log(fib(parseInt(line, 10)));
-    process.exit();
+  console.log(fib(parseInt(line, 10)));
+  process.exit();
 }
 
 function fib(n) {
-    if (n <= 1)
-      return n;
+  if (n <= 1) return n;
 
-    return fib(n - 1) + fib(n - 2);
+  return fib(n - 1) + fib(n - 2);
 }
-console.log(fib(40));
+
+function fasterFib(n) {
+  let a = 0,
+    b = 1,
+    c,
+    i;
+
+  for (i = 2; i < n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  //1.7340252117279783e+41
+  // 1 => last digit
+  return b;
+}
+console.log(fasterFib(200));
+// console.log(fib(200));
 
 module.exports = fib;
